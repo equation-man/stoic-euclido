7bb6e9490588951abce294ecf38c57d85a575adb
equation-man <bigtechguyz@gmail.com>
2025-07-29 09:10:32 +0300

    Added withdrawal support

--- programs/euclid-vault/src/instructions/unlock_funds.rs
+++ programs/euclid-vault/src/instructions/unlock_funds.rs
@@ -1,13 +1,46 @@
 //! THIS IS THE INSTRUCTION TO UNLOCK OR WITHDRAW FUNDS FROM THE VAULT
 use {
     anchor_lang::prelude::*;
-    crate::{state::*, utils::*},
+    crate::{state::*, utils::*, errors::*},
     anchor_spl::{token_interface},
     std::mem::size_of
 };
 
 pub fn handler(ctx: Context<UnlockFunds>, withdraw_amount: u64) -> Result<()> {
     check_token_program(ctx.accounts.token_program.key());
+
+    let user_vault_entry = &ctx.accounts.user_vault_entry;
+    let balance = user_vault_entry.balance;
+    let decimals = ctx.accounts.token_mint.decimals;
+
+    msg!("The stake balance is: {}", user_vault_entry.balance);
+    msg!("Withdrawal amount is: {}", withdraw_amount);
+    msg!("The total vault balance is: {}", ctx.accounts.vault_logs.amount);
+
+    // Verify if the user have >= requested withdrawal amount in their stake.
+    if withdraw_amount > user_vault_entry.balance {
+        return Err(EuclidVaultError::InvalidAccountBalance);
+    }
+
+    // Program signer seeds
+    let auth_bump = ctx.accounts.vault_logs.vault_auth_bump;
+    let auth_seeds = &[VAULT_AUTH_SEED.as_bytes(), &[auth_bump]];
+    let signer = &[&auth_seeds[..]];
+
+    // Transfer tokens
+    transfer_checked(ctx.accounts.transfer_checked_ctx(signer), amount, decimals)?;
+
+    // Borrow mutable reference to update the state of the accounts
+    let vault_logs = &mut ctx.accounts.vault_logs;
+    let user_entry = &mut ctx.accounts.user_vault_entry;
+
+    // Subract the transferred amount from the pool total.
+    vault_logs.amount = vault_logs.amount.checked_sub(withdrawa_amount).unwrap();
+    msg!("The total vault balance is: {}", vault_logs.amount);
+
+    // Update the stake entry.
+    user_entry.balance = user_entry.balance.checked_sub(withdrawal_amount).unwrap();
+    user_entry.created_at = Clock::get().unwrap().unix_timestamp;
 
     Ok(())
 }
@@ -63,4 +96,15 @@ pub struct UnlockFunds<'info> {
 
 impl<'info> UnlockFunds<'info>{
     // transfer_checked for Token22
+    pub fn transfer_checked_ctx<'a>($'a self, seeds: &'a [&[&[u8]]]) -> CpiContext<'_, '_, '_, 'info, TransferChecked<'info>> {
+        let cpi_program = self.token_program.to_account_info();
+        let cpi_accounts = TransferChecked {
+            from: self.token_vault.to_account_info(),
+            to: self.user_token_account.to_account_info(),
+            authority: self.vault_authority.to_account_info(),
+            mint: self.token_mint.to_account_info(),
+        };
+
+        CpiContext::new_with_signer(cpi_program, cpi_accounts, seeds)
+    }
 }
